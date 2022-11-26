476a866dcca06af4a525d53ea27681fd508b87df
mpapenbr <Markus.Papenbrock+github@gmail.com>
2022-11-26 22:53:27 +0000

    chg: preparations for copy event (#16)

--- src/iracelog_service_manager/manager/registry.py
+++ src/iracelog_service_manager/manager/registry.py
@@ -1,13 +1,14 @@
 
 import dataclasses
 from dataclasses import dataclass
+from datetime import datetime
 
 from autobahn.asyncio.wamp import ApplicationSession
 from autobahn.asyncio.wamp import Session
 
 from iracelog_service_manager.manager.commands import CommandType
 from iracelog_service_manager.manager.commands import ManagerCommand
-from iracelog_service_manager.model.eventlookup import EventLookup
+from iracelog_service_manager.model.eventlookup import EventLookup, ReplayInfo
 from iracelog_service_manager.model.eventlookup import ProviderData
 from iracelog_service_manager.persistence.service import session_process_new_event
 from iracelog_service_manager.persistence.service import session_store_event_extra_data
@@ -16,11 +17,10 @@ from iracelog_service_manager.persistence.service import session_store_event_ext
 @dataclass
 class Registry:
     """handles registration of race data providers"""
-    appSession : ApplicationSession
+    appSession: ApplicationSession
     """holds the WAMP session"""
     events: EventLookup
     """lookup for provider"""
-
 
     def __post_init__(self):
         self.appSession.register(self.register_provider, 'racelog.dataprovider.register_provider')
@@ -28,28 +28,36 @@ class Registry:
         self.appSession.register(self.process_event_extra_data, 'racelog.dataprovider.store_event_extra_data')
         self.appSession.register(self.list_providers, 'racelog.public.list_providers')
 
-    def register_provider(self, data:any):
+    def register_provider(self, data: any):
         """
         handles registration of a provider
         """
-        # print(f"{data}")        
-        x = ProviderData(eventKey=data['eventKey'],manifests=data['manifests'], info=data['info'])
+        # print(f"{data}")
+        replayInfo: ReplayInfo = None
+        recordDate: float = None
+        if 'replayInfo' in data:
+            replayInfo = data['replayInfo']
+        if 'recordDate' in data:
+            recordDate = data['recordDate']
+
+        print(f"registerProvider: replayInfo {replayInfo}")
+        x = ProviderData(eventKey=data['eventKey'], manifests=data['manifests'],
+                         info=data['info'], replayInfo=replayInfo,
+                         recordDate=recordDate)
         # print(x)
         if data['eventKey'] in self.events.lookup:
             raise Exception("already there")
         self.events.lookup[data['eventKey']] = x
-        # store 
+        # store
         session_process_new_event(x)
-        # print(x.__dict__)        
+        # print(x.__dict__)
         # self.appSession.publish("racelog.manager.provider", {'eventType': 'new', 'eventData': dataclasses.asdict(x)})
         tosend = ManagerCommand(type=CommandType.REGISTER, payload=x.__dict__).__dict__
-        
+
         # print(tosend)
         self.appSession.publish("racelog.manager.provider", tosend)
-        # TODO: create db entry
-        # TODO: announce new provider
-    
-    def remove_provider(self, eventKey:str):
+
+    def remove_provider(self, eventKey: str):
         """
         handles the removal of a provider
         """
@@ -62,7 +70,7 @@ class Registry:
         else:
             return f"No provider for {eventKey} "
 
-    def process_event_extra_data(self, eventKey:str, extraData:dict={}):
+    def process_event_extra_data(self, eventKey: str, extraData: dict = {}):
         """
         the provider may send extra data. this is handled here
         """
@@ -72,12 +80,9 @@ class Registry:
 
         else:
             return f"No provider for {eventKey} "
-    
-    
 
-    
     def list_providers(self) -> list[ProviderData]:
-        
+
         ret = [dataclasses.asdict(v) for v in self.events.lookup.values()]
         print(ret)
         return ret
--- tests/test_db_access.py
+++ tests/test_db_access.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlalchemy.orm import Session
 
 from iracelog_service_manager.db.schema import Event
@@ -5,14 +6,13 @@ from iracelog_service_manager.persistence.access import read_events
 from iracelog_service_manager.persistence.util import orm_session
 
 
-def test_read_events(testOrm:Session):
+def test_read_events(testOrm: Session):
     print(testOrm)
     res = read_events(testOrm)
     print(res)
 
 
-
-def test_write_event(testOrm:Session):
+def test_write_event(testOrm: Session):
     e = Event(name="Hallo")
     testOrm.add(e)
     # testOrm.commit()
@@ -21,4 +21,13 @@ def test_write_event(testOrm:Session):
     assert res[0].name == "Hallo"
 
 
-
+def test_write_event_with_custom_record_date(testOrm: Session):
+    ts = datetime.fromtimestamp(1668961042.0)
+    e = Event(name="Hallo", description="Desc", recordDate=ts)
+    testOrm.add(e)
+    testOrm.commit()
+    res = read_events(testOrm)
+    assert e == res[0]
+    assert res[0].name == "Hallo"
+    assert res[0].description == "Desc"
+    assert res[0].recordDate == ts
